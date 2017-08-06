9d32bb7d44af67b3303c804bcd96d3056fb2f423
Kelvin Wahome <kevowahome@gmail.com>
2017-08-06 18:38:26 +0300

    Comments

--- multiples_in_range/ruby/sum_multiples_of_3_and_5.rb
+++ multiples_in_range/ruby/sum_multiples_of_3_and_5.rb
@@ -1,4 +1,17 @@
 require_relative 'lib/number_multiples'
+
+# def sum_of_multiples(n=0,d=0)
+#   multiples = (n-1)/d
+#   return (multiples*(multiples+1)/2) * d
+# end
+#
+# t = gets.strip.to_i
+#
+# 1.upto(t) do
+#   n = gets.strip.to_i
+#   puts sum_of_multiples(n, 3) + sum_of_multiples(n, 5) - sum_of_multiples(n, 3*5)
+# end
+
 n = gets.strip.to_i
 sum = NumberMultiples.new(n)
 
