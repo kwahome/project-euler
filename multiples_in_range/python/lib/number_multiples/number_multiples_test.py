bf5efff4782bd47a4a7c1dee7dd36b9c53831453
Kelvin Wahome <kevowahome@gmail.com>
2017-08-07 13:54:37 +0300

    Added methods to find gcd and lcm and list common multiples

--- multiples_in_range/python/lib/number_multiples/number_multiples_test.py
+++ multiples_in_range/python/lib/number_multiples/number_multiples_test.py
@@ -5,10 +5,19 @@ from lib.number_multiples.number_multiples import NumberMultiples
 class NumberMultiplesTest(unittest.TestCase):
     """Test class NumberMultiples"""
     def setUp(self):
+        """
+        This method is called before each test
+        """
         self.multiples = NumberMultiples(n=10)
 
+    def tearDown(self):
+        """
+        This method is called after each test
+        """
+        pass
+
     def test_count(self):
-        self.assertEqual(self.multiples.count_multiples_of('a'), '[ValueError]: a is not a number')
+        self.assertRaises(TypeError, lambda: self.multiples.count_multiples_of('a'))
         self.assertEqual(self.multiples.count_multiples_of(3), 3)
         self.assertEqual(self.multiples.count_multiples_of('3'), 3)
 
@@ -18,14 +27,14 @@ class NumberMultiplesTest(unittest.TestCase):
             2: 6,
             3: 9
         }
-        self.assertEqual(self.multiples.list_multiples_of('a'), '[ValueError]: a is not a number')
+        self.assertRaises(TypeError, lambda: self.multiples.list_multiples_of('a'))
         self.assertEqual(self.multiples.list_multiples_of(3), expected)
         self.assertEqual(self.multiples.list_multiples_of('3'), expected)
 
     def test_sum(self):
-        self.assertEqual(self.multiples.sum_multiples_of('a'), '[ValueError]: a is not a number')
+        self.assertRaises(TypeError, lambda: self.multiples.sum_multiples_of('a'))
         self.assertEqual(self.multiples.sum_multiples_of(3), 18)
         self.assertEqual(self.multiples.sum_multiples_of('3'), 18)
-        self.assertEqual(self.multiples.sum_multiples_of('a', 5), '[ValueError]: a is not a number')
+        self.assertRaises(TypeError, lambda: self.multiples.sum_multiples_of('a', 5))
         self.assertEqual(self.multiples.sum_multiples_of('3', 5), 23)
         self.assertEqual(self.multiples.sum_multiples_of('3', '5'), 23)
